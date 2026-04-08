fd641ff65342710789b20031771887f1fa18f0dd
yangfuzhang <yangfuzhang0720@126.com>
2026-04-09 03:35:25 +0800

    [feat] 修复数据库

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,10 +4,8 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { drizzle } from "drizzle-orm/d1";
 import * as schema from "@db/schema";
 
-const db = drizzle(env.DB, { schema });
-
 export const auth = betterAuth({
-  database: drizzleAdapter(db, { provider: "sqlite" }),
+  database: drizzleAdapter(drizzle(env.DB, { schema }), { provider: "sqlite" }),
   baseURL: env.BETTER_AUTH_URL,
   secret: env.BETTER_AUTH_SECRET,
   account: {
